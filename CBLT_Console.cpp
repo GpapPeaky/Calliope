348fa7fff1dd8f78311cab34af2fb3f4b4f53cb9
GpapPeaky <giorgospapamat@gmail.com>
2026-02-01 16:27:19 +0200

    Fixed console's directive file-switch context error messages

--- CBLT_Console.cpp
+++ CBLT_Console.cpp
@@ -226,25 +226,30 @@ namespace CBLT {
             }
 
         } else { // Directive file-switch context
-            // FIXME: Works but needs some tinkering
             for (auto& entry : cwdContents) {
                 if (entry.n == directiveLine) {
                     f.Load(entry.n); // Load the new file
                     
                     Q.LoadFileToQueue(f); // Add it to the queue
-                } else { // File not fount for file-switch
-                    dr.message = "CBLT_ERR: UNKNOWN FILE TO SWITCH TO " + directiveLine;
-                    std::cout << entry.n << "\n";
-                    std::cout << directiveLine << "\n";
-                    dr.messageType = ConsoleMessage::DIRECTIVE_ERROR;
 
+                    dr.message = "CBLT_LOG: SWITCHED TO " + directiveLine;
+                    dr.messageType = ConsoleMessage::INFO;
+        
                     dirRes = dr;
-                    
-                    directive.Clear();
 
-                    continue;
+                    directive.Clear();
+                    
+                    return; // Early exit
                 }
             }
+
+            // File not fount for file-switch fallback
+            dr.message = "CBLT_ERR: UNKNOWN FILE TO SWITCH TO " + directiveLine;
+            dr.messageType = ConsoleMessage::DIRECTIVE_ERROR;
+
+            dirRes = dr;
+                    
+            directive.Clear();
         }
         
         directive.Clear();
